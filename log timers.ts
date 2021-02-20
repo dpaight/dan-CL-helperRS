ec4c0a0504a6da43863d7cf41226c73a32d72bcd
dan <paight@gmail.com>
2021-02-20 08:23:33 -0800

    first commit

---
+++ log timers.ts
@@ -0,0 +1,16 @@
+// Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
+// Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
+// Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
+// Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
+// fname = arguments.callee.toString().match(/function ([^\(]+)/)[1]
+/**
+ * @param fname (arguments.callee.toString().match(/function ([^\(]+)/)[1])
+ *
+ */
+function lt_logLogTimeStart(funcName) {
+    var d1 = new Date();
+}
+function lt_logLogTimeEnd(funcName) {
+    var d2 = new Date();
+}
+//# sourceMappingURL=module.jsx.map
